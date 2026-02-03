cdc0be0d81137c963528094bd3a29b28b256538a
SunroseMystic <vfosshop@gmail.com>
2026-02-03 15:01:22 -0800

    Update connect.js
    refresh & return URL's added

--- api/stripe/connect.js
+++ api/stripe/connect.js
@@ -3,17 +3,24 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
-  const account = await stripe.accounts.create({
-    type: "standard",
-  });
+  try {
+    const account = await stripe.accounts.create({
+      type: "standard",
+    });
 
-  const accountLink = await stripe.accountLinks.create({
-    account: account.id,
-    refresh_url: "https://bean-me-up-payments.vercel.app/reauth",
-    return_url: "https://bean-me-up-payments.vercel.app/success",
-    type: "account_onboarding",
-  });
+    const accountLink = await stripe.accountLinks.create({
+      account: account.id,
+      refresh_url: "https://bean-me-up-payments.vercel.app/reauth",
+      return_url: "https://bean-me-up-payments.vercel.app/success",
+      type: "account_onboarding",
+    });
 
-  res.redirect(accountLink.url);
+    res.writeHead(302, { Location: accountLink.url });
+    res.end();
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 }
+
+
 
