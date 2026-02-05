a002cc91c80820f936580897dcbcb0eb1ec6cca0
SunroseMystic <vfosshop@gmail.com>
2026-02-04 16:09:18 -0800

    Update checkout.js
    2-4-26 Success page message fix

--- api/checkout.js
+++ api/checkout.js
@@ -34,9 +34,8 @@ export default async function handler(req, res) {
           destination: CONNECTED_ACCOUNT_ID,
         },
       },
-
-      success_url: "https://bean-me-up-payments.vercel.app/success",
-      cancel_url: "https://bean-me-up-payments.vercel.app/cancel",
+success_url: "https://bean-me-up-payments.vercel.app/api/success",
+cancel_url: "https://bean-me-up-payments.vercel.app/api/cancel",
     });
 
     res.redirect(303, session.url);
